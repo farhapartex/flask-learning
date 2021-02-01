54f431d9e8a2c63ab4667a0ab8c7b1f34846a600
Md Nazmul Hasan <nazmul.hasan@circlefintech.com>
2021-02-01 13:22:49 +0600

    flask initial and routing

---
+++ endpoint/path.py
@@ -0,0 +1,27 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "<h1>Hello World!</h1>"
+
+
+@app.route("/home")
+def home():
+    return "<h1>You are in home page</h1>"
+
+
+@app.route("/home/<place>")
+def home_in_place(place):
+    return "<h1>You are in " + place + " now"
+
+
+@app.route("/method", methods=["GET", "POST"])
+def http_method():
+    return "<h1>Testing http method</h1>"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
